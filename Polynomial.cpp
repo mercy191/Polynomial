62fe42cf5e8999875e50f9d240349c1c3a0cf879
mercy191 <ya.nikitaivanov.kz@gmail.com>
2023-11-27 16:50:42 +0700

    Переделан оператор деления.

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -3,8 +3,10 @@
 
 int main()
 {
-    Polynomial poly1 = { 1 , 2, 3, 4 };
-    Polynomial poly2 = { 5 , 6, 7, 8 };
-    Polynomial poly = poly1 * poly2;
+    Polynomial poly1 = { 1, 2, 3, 4 };
+    Polynomial poly2 = { 5, 6, 7, 8 };
+    Polynomial poly = poly1 / poly2;
+    std::cout << poly;
+
 }
 
