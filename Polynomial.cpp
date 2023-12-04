35560bb36fddf609b89e25a07979618508eebf9f
mercy191 <ya.nikitaivanov.kz@gmail.com>
2023-12-05 01:53:28 +0700

    Added accumulation operator overloads, added exceptions and checks.

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -1,12 +1,14 @@
 ﻿#include <iostream>
 #include "Polynomial.h"
 
+
 int main()
 {
     Polynomial poly1 = { 1, 2, 3, 4 };
-    Polynomial poly2 = { 5, 6, 7, 8 };
+    Polynomial poly2 = { 5, 6, 0, 8 };
     Polynomial poly = poly1 / poly2;
-    std::cout << poly;
-
+    std::cout << poly << '\n';
+    poly1 /= 0;
+    std::cout << poly1 << '\n';
 }
 
